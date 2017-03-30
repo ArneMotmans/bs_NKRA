99f16c5f68f5010b072539a741471e0a740cbb52
KevinDominguez <dominguez-kevin-1995@hotmail.com>
2017-03-30 15:20:33 +0200

    RSA cryptografie
    Voorbeeld op rsa

---
+++ Code/RsaCryptoExample/RsaCryptoExample2/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RsaCryptoExample2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+        byte[] plaintext;
+        byte[] encryptedtext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public byte[] Encryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey); encryptedData = RSA.Encrypt(Data, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public byte[] Decryption(byte[] Data, RSAParameters RSAKey, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKey);
+                    decryptedData = RSA.Decrypt(Data, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+
+        private void buttonEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+
+            plaintext = ByteConverter.GetBytes(textBoxTekst.Text);
+            encryptedtext = Encryption(plaintext, RSA.ExportParameters(false), false);
+            textBoxEncrypted.Text = ByteConverter.GetString(encryptedtext);
+        
+     }
+
+        private void buttonDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] decryptedtex = Decryption(encryptedtext, RSA.ExportParameters(true), false);
+            textBoxDecrypted.Text = ByteConverter.GetString(decryptedtex);
+        }
+    }
+}
